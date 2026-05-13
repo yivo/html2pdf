4b3df695d2e3160eb9f6a6ec08d01022c7cd590f
Yaroslav Konoplov <eahome00@gmail.com>
2026-05-13 13:43:04 +0300

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,36 @@
-FROM surnet/alpine-node-wkhtmltopdf:8.11.3-0.12.5-full
+FROM surnet/alpine-node-wkhtmltopdf:22.22.0-0.12.6-small
 
-RUN apk --no-cache add msttcorefonts-installer fontconfig && \
+RUN apk --no-cache add \
+    msttcorefonts-installer \
+    fontconfig \
+    git \
+    ca-certificates \
+    ttf-dejavu \
+    ttf-droid \
+    ttf-freefont \
+    ttf-liberation \
+    ttf-opensans \
+    font-noto \
+    font-noto-cjk && \
     update-ms-fonts && \
-    fc-cache -f
+    update-ca-certificates
 
 COPY fonts /usr/share/fonts
 
-# By default image is built using NODE_ENV=production.
-# You may want to customize it:
-#
-#   --build-arg NODE_ENV=development
-#
-# See https://docs.docker.com/engine/reference/commandline/build/#set-build-time-variables-build-arg
-#
+RUN fc-cache -f
+
 ARG NODE_ENV=production
 ARG APP_PORT=8080
-ENV NODE_ENV=${NODE_ENV} APP_PORT=${APP_PORT} APP_ROOT=/home/app
+ENV NODE_ENV=${NODE_ENV} APP_PORT=${APP_PORT} APP_ROOT=/home/app TZ=UTC
 
-# Expose port to the Docker host, so we can access it from the outside.
 EXPOSE $APP_PORT
-
-# The main command to run when the container starts.
-CMD ["node", "index.js"]
 
 WORKDIR $APP_ROOT
 
-RUN apk add --update curl nano
-
-# Install Gemfile dependencies.
 COPY package.json package-lock.json $APP_ROOT/
-RUN npm install
 
-# Copy application sources.
+RUN npm install --omit=dev
+
 COPY . $APP_ROOT
+
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,76 +1,314 @@
-var _           = require('lodash');
-var express     = require('express');
-var app         = express();
-var fs          = require('fs');
-var temp        = require('temp-write');
-var execute     = require('child_process').exec;
-var shellescape = require('shell-escape');
-var bodyparser  = require('body-parser');
+import "log-timestamp";
+import _ from "lodash";
+import express from "express";
+import bodyparser from "body-parser";
+import crypto from "crypto";
+import fs, { promises as fsPromises } from "fs";
+import { spawn } from "child_process";
+import path from "path";
+import os from "os";
 
-app.use(bodyparser.json({ limit: '128mb' }));
-app.use(bodyparser.urlencoded({ limit: '128mb', extended: true }));
+const app = express();
+app.use(bodyparser.json({ limit: "32mb" }));
 
-app.post('/', function(req, res) {
-  console.log('\n\n[' + new Date() + ']');
-
-  req.setTimeout(24 * 60 * 60 * 1000); // 24 hours.
-
-  function argumentize(name, value) {
-    return '--' + name.replace(/_/g, '-') + ' ' + shellescape([_.toString(value)]);
+async function storeTemporaryFile(contents, extension) {
+  let fileName = crypto.randomUUID()
+  if (extension) {
+    fileName += `.${extension}`
   }
+  const filePath = path.join(os.tmpdir(), fileName);
+  await fsPromises.writeFile(filePath, contents);
+  return filePath;
+}
 
-  var options = _.map(_.omit(req.body, 'html', 'url', 'header_html', 'footer_html'), function(value, name) {
-    return argumentize(name, value);
-  });
+const PDF_GENERATION_TIMEOUT = 24 * 60 * 60 * 1000 // 24 hours
 
-  _.each(['header_html', 'footer_html'], function(name) {
-    if (_.isEmpty(_.trim(req.body[name]))) { return }
-    if (_.toString(req.body[name]).match(/^https?:\/\//)) {
-      options.push(argumentize(name, req.body[name]));
-    } else {
-      var path = temp.sync(_.toString(req.body[name]), 'template.html');
-      var url  = 'file://' + path;
-      setTimeout(function() { fs.unlink(path, _.noop); }, 24 * 60 * 60 * 1000); // 24 hours.
-      options.push(argumentize(name, url));
+function convertOptionNameToAPIParameterFormat(name) {
+  return name.replace(/^--/, "").replace(/-/g, "_")
+}
+
+function convertOptionNameToCommandLineFormat(name) {
+  return "--" + name.replace(/_/g, "-")
+}
+
+const PERMITTED_OPTIONS_BOOLEAN = Object.freeze(_.map([
+  "--collate",
+  "--no-collate",
+  "--grayscale",
+  "--lowquality",
+  "--no-pdf-compression",
+  "--read-args-from-stdin",
+  "--use-xserver",
+  "--dump-default-toc-xsl",
+  "--outline",
+  "--no-outline",
+  "--background",
+  "--no-background",
+  "--custom-header-propagation",
+  "--no-custom-header-propagation",
+  "--debug-javascript",
+  "--no-debug-javascript",
+  "--default-header",
+  "--disable-external-links",
+  "--enable-external-links",
+  "--disable-forms",
+  "--enable-forms",
+  "--images",
+  "--no-images",
+  "--disable-internal-links",
+  "--enable-internal-links",
+  "--disable-javascript",
+  "--enable-javascript",
+  "--keep-relative-links",
+  "--disable-local-file-access",
+  "--enable-local-file-access",
+  "--exclude-from-outline",
+  "--include-in-outline",
+  "--disable-plugins",
+  "--enable-plugins",
+  "--print-media-type",
+  "--no-print-media-type",
+  "--proxy-hostname-lookup",
+  "--resolve-relative-links",
+  "--disable-smart-shrinking",
+  "--enable-smart-shrinking",
+  "--stop-slow-scripts",
+  "--no-stop-slow-scripts",
+  "--disable-toc-back-links",
+  "--enable-toc-back-links",
+  "--footer-line",
+  "--no-footer-line",
+  "--header-line",
+  "--no-header-line",
+  "--disable-dotted-lines",
+  "--disable-toc-links",
+]))
+
+const PERMITTED_OPTIONS_VALUE = Object.freeze(_.map([
+  "--copies",
+  "--dpi",
+  "--image-dpi",
+  "--image-quality",
+  "--margin-bottom",
+  "--margin-left",
+  "--margin-right",
+  "--margin-top",
+  "--orientation",
+  "--page-height",
+  "--page-size",
+  "--page-width",
+  "--title",
+  "--outline-depth",
+  "--bypass-proxy-for",
+  "--encoding",
+  "--javascript-delay",
+  "--load-error-handling",
+  "--load-media-error-handling",
+  "--minimum-font-size",
+  "--page-offset",
+  "--password",
+  "--proxy",
+  "--run-script",
+  "--ssl-key-password",
+  "--username",
+  "--viewport-size",
+  "--window-status",
+  "--zoom",
+  "--footer-center",
+  "--footer-font-name",
+  "--footer-font-size",
+  "--footer-left",
+  "--footer-right",
+  "--footer-spacing",
+  "--header-center",
+  "--header-font-name",
+  "--header-font-size",
+  "--header-left",
+  "--header-right",
+  "--header-spacing",
+  "--toc-header-text",
+  "--toc-level-indentation",
+  "--toc-text-size-shrink",
+], convertOptionNameToAPIParameterFormat))
+
+const PERMITTED_OPTIONS_NAME_VALUE = Object.freeze(_.map([
+  "--cookie",
+  "--custom-header",
+  "--post",
+  "--replace",
+], convertOptionNameToAPIParameterFormat))
+
+const PERMITTED_OPTIONS_FILE_NAME_FILE_PATH = Object.freeze([
+  "--post-file",
+])
+
+const PERMITTED_OPTIONS_FILE_PATH = Object.freeze(_.map([
+  "--header-html",
+  "--footer-html",
+  "--checkbox-checked-svg",
+  "--checkbox-svg",
+  "--radiobutton-checked-svg",
+  "--radiobutton-svg",
+  "--ssl-crt-path",
+  "--user-style-sheet",
+  "--xsl-style-sheet",
+  "--ssl-key-path",
+  "--cookie-jar",
+], convertOptionNameToAPIParameterFormat))
+
+const OPTION_TO_FILE_EXTENSION = Object.freeze({
+  "--header-html": "html",
+  "--footer-html": "html",
+  "--checkbox-checked-svg": "svg",
+  "--checkbox-svg": "svg",
+  "--radiobutton-checked-svg": "svg",
+  "--radiobutton-svg": "svg",
+  "--ssl-crt-path": "cert",
+  "--user-style-sheet": "css",
+  "--xsl-style-sheet": "css",
+  "--ssl-key-path": "pem",
+  "--cookie-jar": "jar",
+})
+
+app.post("/", async (req, res) => {
+  const options = ["--log-level", "warn"]
+  const temporaryFilesToCleanup = []
+
+  try {
+
+    // Body HTML (mandatory)
+    const bodyHTML = _.trim(req.body.body_html)
+    if (bodyHTML === "") {
+      return res.status(422).json({ error: "body_html can't be blank" }).end()
     }
-  });
 
-  if (!_.isEmpty(req.body.url)) {
-    var url = req.body.url;
-  } else {
-    var path = temp.sync(_.toString(req.body.html), 'index.html');
-    var url  = 'file://' + path;
-    setTimeout(function() { fs.unlink(path, _.noop); }, 24 * 60 * 60 * 1000); // 24 hours.
+    for (const optionName of PERMITTED_OPTIONS_BOOLEAN) {
+      let optionValue = req.body[optionName]
+      if (optionValue === null || optionValue === undefined) { continue }
+
+      if (typeof optionValue === "boolean" && optionValue === true) {
+        options.push(convertOptionNameToCommandLineFormat(optionName))
+      }
+    }
+
+    for (const optionName of PERMITTED_OPTIONS_VALUE) {
+      let optionValue = req.body[optionName]
+      if (optionValue === null || optionValue === undefined) { continue }
+
+      options.push(convertOptionNameToCommandLineFormat(optionName), optionValue)
+    }
+
+    for (const optionName of PERMITTED_OPTIONS_FILE_NAME_FILE_PATH) {
+      const optionValue = req.body[optionName]
+      if (!_.isArray(optionValue)) { continue }
+
+      for (const item of optionValue) {
+        if (!_.isArray(item) || item.length !== 2) { continue }
+
+        const filePath = await storeTemporaryFile(item[1])
+        temporaryFilesToCleanup.push(filePath)
+        options.push(convertOptionNameToCommandLineFormat(optionName), item[0], filePath)
+      }
+    }
+
+    for (const optionName of PERMITTED_OPTIONS_FILE_PATH) {
+      const optionValue = req.body[optionName]
+      if (optionValue === null || optionValue === undefined || req.body[optionName] === "") { continue }
+
+      const convertedOptionName = convertOptionNameToCommandLineFormat(optionName)
+      const filePath = await storeTemporaryFile(optionValue, OPTION_TO_FILE_EXTENSION[convertedOptionName])
+      temporaryFilesToCleanup.push(filePath)
+      options.push(convertedOptionName, optionName.endsWith("html") ? `file://${filePath}` : filePath)
+    }
+
+    for (const optionName of PERMITTED_OPTIONS_NAME_VALUE) {
+      const optionValue = req.body[optionName]
+      if (!_.isArray(optionValue)) { continue }
+
+      for (const item of optionValue) {
+        if (!_.isArray(item) || item.length !== 2) { continue }
+
+        options.push(convertOptionNameToCommandLineFormat(optionName), item[0], item[1])
+      }
+    }
+
+    const bodyHTMLFilePath = await storeTemporaryFile(bodyHTML, "html")
+    temporaryFilesToCleanup.push(bodyHTMLFilePath)
+    options.push(bodyHTMLFilePath)
+
+    const pdfFilePath = path.join(os.tmpdir(), `${crypto.randomUUID()}.pdf`)
+    temporaryFilesToCleanup.push(pdfFilePath)
+    options.push(pdfFilePath)
+
+    req.setTimeout(PDF_GENERATION_TIMEOUT);
+    const time = Date.now();
+    console.log(`wkhtmltopdf ${options.join(" ")}`);
+    const pdfProcess = spawn("wkhtmltopdf", options, { timeout: PDF_GENERATION_TIMEOUT });
+
+    pdfProcess.stdout.on("data", (data) => {
+      console.log(`wkhtmltopdf: ${data.toString("UTF-8")}`);
+    });
+
+    pdfProcess.stderr.on("data", (data) => {
+      console.log(`wkhtmltopdf: ${data.toString("UTF-8")}`);
+    });
+
+    req.on("close", () => {
+      if (!res.writableEnded && pdfProcess.exitCode === null) {
+        console.log("Client disconnected prematurely. Killing wkhtmltopdf process...");
+        pdfProcess.kill("SIGKILL");
+      }
+    });
+
+    await new Promise((resolve, reject) => {
+      pdfProcess.on("close", (code) => {
+        if (code === 0) {
+          console.log(`PDF generated in ${Date.now() - time} ms`);
+          resolve();
+        } else {
+          reject(new Error(`wkhtmltopdf exited with code ${code}`));
+        }
+      });
+      pdfProcess.on("error", reject);
+    });
+
+    res.contentType("application/pdf");
+
+    const readStream = fs.createReadStream(pdfFilePath);
+    readStream.pipe(res);
+
+    await new Promise((resolve, reject) => {
+      readStream.on("end", resolve);
+      readStream.on("error", reject);
+    });
+
+  } catch (error) {
+    console.error(error.message)
+
+    // Don't do anything if we have already started to stream PDF contents to client
+    if (!res.headersSent) {
+      res.sendStatus(500);
+    }
+  } finally {
+    for (const filePath of temporaryFilesToCleanup) {
+      try {
+        await fsPromises.unlink(filePath);
+      } catch (error) {
+        console.error(`Failed to delete temporary file ${filePath}:`, error.message);
+      }
+    }
   }
-
-  var command = 'wkhtmltopdf ' + options.join(' ') + ' ' + shellescape([url]) + ' - | cat';
-
-  console.log('Executing ' + command);
-
-  var time = Date.now();
-
-  execute(command, { encoding: 'buffer', timeout: 24 * 60 * 60 * 1000, maxBuffer: 64 * 1024 * 1024 * 1024 }, function(error, stdout, stderr) {
-    stderr = _.trim(stderr.toString('UTF-8'));
-    if (stderr) { console.log(stderr) }
-
-    var size = Buffer.byteLength(stdout);
-    console.log('PDF generator returned ' + size + ' bytes of data.');
-
-    if (error != null || size < 1024) {
-      if (error != null) { console.log(error) }
-      return res.sendStatus(500);
-    }
-
-    console.log('PDF generated in ' + (Date.now() - time) + 'ms.');
-    res.contentType('application/pdf');
-    res.send(stdout);
-  });
 });
 
-var close = _.once(function() { server.close(); process.exit(); });
-_.each(['SIGINT', 'SIGTERM'], function(signal) { process.on(signal, close); });
+const signalHandler = _.once(() => {
+  server.close(() => { process.exit(0) })
+})
 
-var port = process.env.APP_PORT || 8080;
-var server = app.listen(port, function() {
-  console.log('Server is listening on port %s.', port);
-});
+_.each(["SIGINT", "SIGTERM"], (signal) => {
+  process.on(signal, signalHandler)
+})
+
+const port = process.env.APP_PORT || 8080
+const server = app.listen(port, () => {
+  console.log(`Listening on http://0.0.0.0:${port}`)
+})
